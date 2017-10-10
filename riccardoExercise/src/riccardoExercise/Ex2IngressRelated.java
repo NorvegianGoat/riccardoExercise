90591e745411040a44d62b278fba7af2a7b35dc7
shineangelic <shineangelic@gmail.com>
2017-10-10 18:56:42 +0200

    scanner stub

---
+++ riccardoExercise/src/riccardoExercise/Ex2IngressRelated.java
@@ -0,0 +1,11 @@
+package riccardoExercise;
+
+import java.util.Scanner;
+
+public class Ex2IngressRelated {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+	}
+
+}
