b870875bd6cbb5c26af41359d16c4d035186c5ed
CapraNorvegese <riccardomioli@gmail.com>
2017-10-12 22:47:22 +0200

    Aggiornato

--- riccardoExercise/src/riccardoExercise/Shine.java
+++ riccardoExercise/src/riccardoExercise/Shine.java
@@ -5,6 +5,7 @@ public class Shine {
 		/*Presentazione programma*/
 		System.out.println ("Ciao sono Capra, dopo un'ora di ripetizioni di analisi ho ben pensato di fare un programmino in java");
 		System.out.println ("Scrivi la dimensione del lato di un quadrato, io calcolero' le dimensioni dell'area");
+		/*Calcolo e presentazione risultato*/
 		Scanner in = new Scanner(System.in);
 		double secondoNumero =	in.nextDouble ();
 		System.out.println ("Calcolo l'area");
